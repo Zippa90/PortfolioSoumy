bc45df4e32aea9698159e7655cc272aada612a65
zippa90 <zippa.12@gmail.com>
2022-07-06 11:33:40 +0300

    Home -> Start

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -55,7 +55,7 @@ function NavBar() {
           <Nav className="ms-auto" defaultActiveKey="#home">
             <Nav.Item>
               <Nav.Link as={Link} to="/" onClick={() => updateExpanded(false)}>
-                <AiOutlineHome style={{ marginBottom: "2px" }} /> Home
+                <AiOutlineHome style={{ marginBottom: "2px" }} /> Start
               </Nav.Link>
             </Nav.Item>
 
